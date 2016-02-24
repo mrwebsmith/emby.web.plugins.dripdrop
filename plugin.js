60a6f1cef4d740ab3d87fef82d2602e11fa31680
mrwebsmith <mrwebsmith@gmail.com>
2016-02-24 16:25:22 -0500

    new name and id

--- plugin.js
+++ plugin.js
@@ -4,9 +4,9 @@ define([], function () {
 
         var self = this;
 
-        self.name = 'Coins';
+        self.name = 'DripDrop';
         self.type = 'soundeffects';
-        self.id = 'coinsoundeffects';
+        self.id = 'dripsoundeffects';
 
         self.getEffects = function () {
             return {
